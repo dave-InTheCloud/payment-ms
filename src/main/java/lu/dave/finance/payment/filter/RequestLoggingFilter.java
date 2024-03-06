9976a73ffbf4a70c874724cc1347b87d4112edc6
dave-InTheCloud <davidlambiase@gmail.com>
2024-03-07 00:14:54 +0100

    add spring filter for logging

---
+++ src/main/java/lu/dave/finance/payment/filter/RequestLoggingFilter.java
@@ -0,0 +1,26 @@
+package lu.dave.finance.payment.filter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@Order(1)
+@Slf4j
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, java.io.IOException {
+        // Log request details (method, URI, headers, etc.)
+        log.info("Request: {} {}", request.getMethod(), request.getRequestURI());
+        filterChain.doFilter(request, response);
+    }
+}
